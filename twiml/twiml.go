fa34aef54e14a6d5ff8a7b249a4a9e0f9ca6650c
Josh Matthews <jmatthews@twilio.com>
2014-07-08 16:03:22 -0700

    Changing Render to return a Reader instead of a string

--- twiml/twiml.go
+++ twiml/twiml.go
@@ -1,9 +1,13 @@
 package twiml
 
-import "encoding/xml"
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+)
 
 type TwimlInterface interface {
-	Render() (string, error)
+	Render() (io.Reader, error)
 	Say(SayOpts, ...string) TwimlInterface
 	Play(PlayOpts, ...string) TwimlInterface
 	// Dial(...string) TwimlInterface
@@ -21,13 +25,15 @@ type Twiml struct {
 	contents []interface{}
 }
 
-// Returns a string representation of the TwiML generated previous calls to this object.
-func (t Twiml) Render() (string, error) {
+// Returns a TwiML representation of the previous calls on the struct, contained
+// inside a Reader interface.
+func (t Twiml) Render() (io.Reader, error) {
 	result, err := xml.MarshalIndent(t.contents, "", "  ")
-	return string(result), err
+	return bytes.NewReader(result), err
 }
 
 // Say some text during a phone call.
+//
 // https://www.twilio.com/docs/api/twiml/say
 func (t *Twiml) Say(opts SayOpts, lines ...string) TwimlInterface {
 	for _, line := range lines {
@@ -38,6 +44,7 @@ func (t *Twiml) Say(opts SayOpts, lines ...string) TwimlInterface {
 }
 
 // Play an audio file during a phone call.
+//
 // https://www.twilio.com/docs/api/twiml/play
 func (t *Twiml) Play(opts PlayOpts, urls ...string) TwimlInterface {
 	for _, url := range urls {
@@ -48,6 +55,7 @@ func (t *Twiml) Play(opts PlayOpts, urls ...string) TwimlInterface {
 }
 
 // Record audio during a phone call.
+//
 // https://www.twilio.com/docs/api/twiml/record
 func (t *Twiml) Record(opts RecordOpts, action string) TwimlInterface {
 	newRecord := &record{0, action, &opts}
