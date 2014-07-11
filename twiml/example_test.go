16dcfd4be21d0c6923f38f16bd8c3665924335ed
Josh Matthews <jmatthews@twilio.com>
2014-07-11 15:25:27 -0700

    Adding example using twiml.Response as http.Hanlder

--- twiml/example_test.go
+++ twiml/example_test.go
@@ -2,6 +2,7 @@ package twiml_test
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/natebrennand/twiliogo/twiml"
 )
@@ -18,4 +19,13 @@ func Example() {
 	// 	<Say voice="alice">My hands are typing words</Say>
 	// 	<Say voice="alice">Haaaaaaaaaaaaands</Say>
 	// </Response>
+}
+
+// twiml.Response implements the http.Handler interface, so you can pass
+// your prepared response directly to http to handle a route.
+func ExampleResponse() {
+	response := new(twiml.Response)
+	response.Say(twiml.SayOpts{Voice: "alice"}, "Hi there!", "I'm responding to an HTTP request.")
+	http.Handle("/callback/voice/", response)
+	http.ListenAndServe(":8080", nil)
 }
