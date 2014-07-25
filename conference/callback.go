a146adab7cb001a149e4ead714d2f76b5cd84694
Nate Brennand <natebrennand@gmail.com>
2014-07-24 23:50:52 -0700

    remove err check since it's not possible

--- conference/callback.go
+++ conference/callback.go
@@ -23,11 +23,7 @@ func (cb *Callback) Parse(req *http.Request) error {
 func CallbackHandler(callbackChan chan Callback) http.HandlerFunc {
 	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 		var cb Callback
-		err := cb.Parse(req)
-		if err != nil {
-			resp.WriteHeader(http.StatusBadRequest)
-			return
-		}
+		cb.Parse(req)
 		resp.WriteHeader(http.StatusOK)
 		go func() {
 			callbackChan <- cb
