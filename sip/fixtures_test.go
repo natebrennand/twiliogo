85282b053e2718cb14e1e7b07b3681672a84cdae
Chelsea <chelpu@umich.edu>
2014-07-28 13:02:28 -0700

    adding fixtures test

--- sip/fixtures_test.go
+++ sip/fixtures_test.go
@@ -230,4 +230,152 @@ var (
 			Credentials: "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/CredentialLists/CL32a3c49700934481addd5ce1659f04d2/Credentials.json",
 		},
 	}
+
+	testIPAccessControlListsString = `{
+		"first_page_uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists.json?PageSize=50&Page=0",
+	    "total": 1,
+	    "end": 0,
+	    "previous_page_uri": null,
+	    "uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/SIP/IpAccessControlLists.json?PageSize=50&Page=0",
+	    "page_size": 50,
+	    "num_pages": 1,
+	    "start": 0,
+	    "next_page_uri": null,
+	    "ip_access_control_lists": [
+	        {
+	            "subresource_uris": {
+	                "credentials": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IPAccessControlLists.json"
+	            },
+	            "date_updated": "Wed, 11 Sep 2013 17:51:38 -0000",
+	            "friendly_name": "Production Gateways IP Address - Scranton",
+	            "uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IPAccessControlLists/AL32a3c49700934481addd5ce1659f04d2.json",
+	            "account_sid": "AC32a3c49700934481addd5ce1659f04d2",
+	            "sid": "AL32a3c49700934481addd5ce1659f04d2",
+	            "date_created": "Wed, 11 Sep 2013 17:51:38 -0000"
+	        },
+	    ],
+	    "last_page_uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists.json?PageSize=50&Page=0",
+	    "page": 0
+	}`
+
+	testIPAccessControlLists = IPAccessControlLists{
+		ListResponseCore: common.ListResponseCore{
+			Start:        0,
+			Total:        1,
+			NumPages:     1,
+			Page:         0,
+			PageSize:     50,
+			End:          0,
+			URI:          "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IPAccessControlLists.json?PageSize=50&Page=0",
+			FirstPageURI: "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IPAccessControlLists.json?PageSize=50&Page=0",
+			LastPageURI:  "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IPAccessControlLists.json?PageSize=50&Page=0",
+		},
+		ControlLists: &[]IPAccessControlList{
+			IPAccessControlList{
+				ResourceInfo: common.ResourceInfo{
+					Sid:         "AL32a3c49700934481addd5ce1659f04d2",
+					URI:         "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL32a3c49700934481addd5ce1659f04d2.json",
+					DateCreated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+					DateUpdated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+				},
+				SubresourceURIs: addressesSubresourceURI{
+					Addresses: "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL32a3c49700934481addd5ce1659f04d2/Addresses.json",
+				},
+			},
+		},
+	}
+
+	testIPAccessControlListString = `{
+	    "subresource_uris": {
+	        "addresses": "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json"
+	    }, 
+	    "date_updated": "Wed, 11 Sep 2013 04:06:07 -0000", 
+	    "friendly_name": "My new acl", 
+	    "uri": "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7.json", 
+	    "sid": "AL0c37d0be69a6a6fe1e270c5fa4a2cac7", 
+	    "date_created": "Wed, 11 Sep 2013 04:06:07 -0000"
+	}`
+
+	testIPAccessControlList = IPAccessControlList{
+		ResourceInfo: common.ResourceInfo{
+			Sid:         "AL0c37d0be69a6a6fe1e270c5fa4a2cac7",
+			URI:         "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7.json",
+			DateCreated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+			DateUpdated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+		},
+		SubresourceURIs: addressesSubresourceURI{
+			Addresses: "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json",
+		},
+	}
+
+	testIPAddressResourceString = `{
+	    "date_updated": "Wed, 11 Sep 2013 04:32:30 -0000",
+	    "friendly_name": "My office IP Address",
+	    "uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses/IP6fbe2e8619a14fabaefaf4fb131c0b9d.json",
+	    "sid": "IP6fbe2e8619a14fabaefaf4fb131c0b9d",
+	    "date_created": "Wed, 11 Sep 2013 04:32:30 -0000",
+	    "ip_address": "55.102.123.124"
+	}`
+
+	testIPAddressResource = IPAddressResource{
+		ResourceInfo: common.ResourceInfo{
+			Sid:         "AL0c37d0be69a6a6fe1e270c5fa4a2cac7",
+			URI:         "/2010-04-01/Accounts/AC1fcc43cc0b4157cae6b77cdb692b437e/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7.json",
+			DateCreated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+			DateUpdated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+		},
+		FriendlyName: "My office IP Address",
+		IPAddress:    "55.102.123.124",
+	}
+
+	testIPAddressListString = `{
+	    "first_page_uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json?PageSize=50&Page=0",
+	    "end": 0,
+	    "previous_page_uri": null,
+	    "ip_addresses": [
+	        {
+	            "date_updated": "Wed, 11 Sep 2013 04:32:30 -0000",
+	            "friendly_name": "My office IP Address",
+	            "uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses/IP6fbe2e8619a14fabaefaf4fb131c0b9d.json",
+	            "sid": "IP6fbe2e8619a14fabaefaf4fb131c0b9d",
+	            "date_created": "Wed, 11 Sep 2013 04:32:30 -0000",
+	            "ip_address": "55.102.123.124"
+	        },
+	        ...
+	    ],
+	    "uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json?PageSize=50&Page=0",
+	    "page_size": 50,
+	    "num_pages": 1,
+	    "start": 0,
+	    "next_page_uri": null,
+	    "last_page_uri": "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json?PageSize=50&Page=0",
+	    "total": 1,
+	    "page": 0
+	}`
+
+	testIPAddressList = IPAddressList{
+		ListResponseCore: common.ListResponseCore{
+			Start:        0,
+			Total:        1,
+			NumPages:     1,
+			Page:         0,
+			PageSize:     50,
+			End:          0,
+			URI:          "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json?PageSize=50&Page=0",
+			FirstPageURI: "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json?PageSize=50&Page=0",
+			LastPageURI:  "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses.json?PageSize=50&Page=0",
+		},
+		IPAddresses: &[]IPAddressResource{
+			IPAddressResource{
+				ResourceInfo: common.ResourceInfo{
+					Sid:         "IP6fbe2e8619a14fabaefaf4fb131c0b9d",
+					URI:         "/2010-04-01/Accounts/AC32a3c49700934481addd5ce1659f04d2/SIP/IpAccessControlLists/AL0c37d0be69a6a6fe1e270c5fa4a2cac7/Addresses/IP6fbe2e8619a14fabaefaf4fb131c0b9d.json",
+					DateCreated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+					DateUpdated: common.JSONTime{Time: time.Date(2010, time.August, 18, 20, 1, 40, 0, &time.Location{})},
+				},
+				FriendlyName: "My office IP Address",
+				IPAddress:    "55.102.123.124",
+			},
+		},
+	}
 )
